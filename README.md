a31d0c4d639dc47f4b65079d33d2e6fa819bc7fe
KarolHul <99616289+KarolHul@users.noreply.github.com>
2022-06-13 00:51:35 +0200

    Update code and README

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # RMFF
  Random Meme From Folder
+
+Configuration
+You need to create configuration file. The general syntax for this file is:
+{
+"Pictures":"path_to_folder",
+"Videos":"path_to_folder"
+}
--- randomizer.php
+++ randomizer.php
@@ -2,17 +2,25 @@
 $json=file_get_contents("config_file.json");
  $decode = json_decode($json,true);
 
- function random($var,$type){
+ function random($type,$amount = "0"){
      global $decode;
      $a = $decode["$type"];
     $var = scandir($a);
     unset($var[0]);
     unset($var[1]);
     $var = array_values($var);
-    $rand = array_rand($var);
-    return "$a/$var[$rand]";
+    shuffle($var);
+    $aarray = array_slice($var,0,$amount);
+    foreach($aarray as $value){
+      $value = $a ."/". $value;
+      array_shift($aarray);
+      array_push($aarray,$value);
+    }
+    return $aarray;
 }
-$P = random("pic","Pictures");
-$V = random("vid","Videos");
-$G = random("gif","Gifs");
+function printer($array){
+     foreach($array as $value){
+         echo $value . "\n";
+     }
+}
 ?>
